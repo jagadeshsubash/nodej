27398e28b083bacdaff4630ab15bbe22118cc5cf
Jagadesh <32300213+jagadeshsubash@users.noreply.github.com>
2022-03-06 17:03:05 +0530

    Add files via upload

---
+++ index (1).js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+
+fs.readFile('test.txt', 'utf8' , (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  console.log(data)
+
+})
+console.log("consoledata");
